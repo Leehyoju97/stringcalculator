00be2ef35a5cb6abd2c04aec276be491b5db851a
Leehyoju97 <dlgywn2420@gmail.com>
2022-11-05 21:24:02 +0900

    feat: 문자열 계산기

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,5 +1,11 @@
+import domain.Calculator;
+import domain.Expression;
+import view.InputView;
+import view.OutputView;
+
 public class MainApplication {
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator(new Expression(InputView.showInputMessage()));
+        OutputView.showOutputMessage(calculator.calculate());
     }
 }
